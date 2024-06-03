bfe6efeb357c8abc2a3dada70ae8e70ca0c43a99
Frederick <sf20@keeling.earth.illinois.edu>
2024-06-03 15:14:43 -0500

    Add method for plotting mass fraction in 2x2 panel at regular 2hr intervals

--- analysis/griddedoutput_plotting.py
+++ analysis/griddedoutput_plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from cycler import cycler
 #from griddedoutput_loaddatasets import *
 from loaddatastructs import * # use GriddedOutput data object
+from griddedoutput_helperfuncs import *
 plt.rcParams['axes.prop_cycle'] = cycler('color', plt.get_cmap('tab20').colors)
 
 def plotNumberDist(number_dist_arr, plot_type='hist', **kwargs):
@@ -135,3 +136,81 @@ def construct_figure_filename(figure_type, **kwargs):
     extension = kwargs.get('figure_extension', 'pdf')
     filename += f'.{extension}'
     return filename
+
+def plotFourPanelMassFrac(scenario, times, xstart, xend, ystart, yend, z_idx, **kwargs):
+    
+    fig, axs  = plt.subplots(2,2, figsize=(6.5,4.5))
+    plt.subplots_adjust(hspace=.3, wspace=.2)
+
+    if len(times) != 4:
+        raise AttributeError("Number of times must be four")
+        
+    for j, (ax, t) in enumerate(zip(axs.flatten(), times)):
+
+        GriddedOutput.loadData(scenario, xstart, xend, ystart, yend, z_idx, t)
+        binned_species_mass_arr =  getBinnedSpeciesMassOptimized(GriddedOutput.gridded_data[scenario]['aero_diams'],
+                                                                GriddedOutput.gridded_data[scenario]['aero_masses'], 
+                                                                GriddedOutput.gridded_data[scenario]['aero_numconc'], 
+                                                                n_grid_cells=GriddedOutput.gridded_data[scenario]['n_total_cells'])
+
+
+        if binned_species_mass_arr.shape[1] == GriddedOutput.n_bins:
+            binned_species_mass_arr = binned_species_mass_arr[:, :-1]
+
+        total_mass_per_bin = (binned_species_mass_arr[:, :]#/bin_logwidth # moved division by logwidth to calculation of mass distrib
+                            ).sum(axis=0)
+        frac_total = np.zeros((GriddedOutput.n_bins-1,))
+
+        # Plot the total speciated mass distribution
+        aero_species = [var for var in GriddedOutput.aero_vars if 'pmc_' in var]
+        for i, species in enumerate(aero_species):
+            species_mass_dist = binned_species_mass_arr[i, :]#/bin_logwidth # moved division by logwidth to calculation of mass distrib
+            species_frac = species_mass_dist/total_mass_per_bin
+            frac_total += species_frac
+            if i == 0:
+                y_lower = 0
+                y_lower = np.array((GriddedOutput.n_bins-1)*[y_lower])
+            else:
+                y_lower = frac_total - species_frac
+
+            variable_fmt = GriddedOutput.aerosol_fmt_map[species]
+            variable_fmt = variable_fmt.replace('Aerosol ', '')
+            if ('ARO' in species) or ('ALK' in species) or ('OLE' in species) or ('API' in species) or ('LIM' in species):
+                variable_fmt = variable_fmt.replace('_', '')
+            if variable_fmt in ['CL', 'NA', 'CA']:
+                variable_fmt = variable_fmt.title()
+            ax.fill_between(x=GriddedOutput.bin_geocenter, y1=y_lower, y2=frac_total, label=variable_fmt)
+
+        # set xlim, ylim, axis scaling
+        ax.set_xlim(GriddedOutput.bin_edges[0], GriddedOutput.bin_edges[-1])
+        ax.set_ylim(0, 1)
+        ax.set_xscale('log')
+        if kwargs.get('xlims'):
+            ax.set_xlim(kwargs.get('xlims'))
+        
+        # add legend
+        if j == 0:
+            fig.legend(fontsize=11, ncol=5, loc='center', bbox_to_anchor=(.5,-.15))
+
+        # Set x-axis ticks and labels
+        if j < 2:
+            ax.set_xticklabels([])
+        else:
+            ax.set_xlabel('Particle Diameter [m]', fontsize=11)
+
+        # Set y-axis ticks and labels
+        if j in [1,3]:
+            ax.set_yticklabels([])
+        else:
+            # Set y-axis ticks and label
+            ax.set_ylabel('Mass Fraction', fontsize=11)  
+
+        ax.tick_params(axis='both', which='major', labelsize=11)
+
+        # add subplot title (time)
+        history_dt = GriddedOutput.historydelta_m/60 # hours
+        time =  ((t-1)*history_dt)
+        ax.set_title(f'$t = {time:3.0f}$ h', fontsize=11.5)
+
+    if kwargs.get("savefig"):
+        plt.savefig(f'speciated-mass-frac-four-panel-{scenario}-z{z_idx}.pdf', format='pdf', bbox_inches='tight')
